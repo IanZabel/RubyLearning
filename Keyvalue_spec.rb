7ca2bbc2e9a05b1bdf72b8b241d71e0c7ede3761
Ian B. Zabel <09zabeli@gmail.com>
2018-12-01 14:01:09 -0800

    Update Keyvalue_spec.rb

--- Keyvalue_spec.rb
+++ Keyvalue_spec.rb
@@ -3,14 +3,12 @@ require './Keyvalue.rb'
 describe 'Key-Value Response Test' do
 
 	it "sends 'put a b' and recieves ok as output" do
-		
-		input = StringIO.new("put a b\r\n")
-		output = Keyvalue.new.mainloop(input)
-		output.should eq "ok"
 
-		input = StringIO.new("exit\r\n")
-		output = Keyvalue.new.mainloop(input)
-		output.should eq 'exit'
+		input = StringIO.new("put a b\r\nexit\r\n")
+		expectedoutput = "ok\r\nBye!\r\n"
+		actualoutput = StringIO.new
+		Keyvalue.new.mainloop(input, actualoutput)
+		expect(actualoutput.read).to eq expectedoutput
 
 	end
 end
