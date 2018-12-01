254ec1ffb615412120e80e18b156307b7aa2cde0
Ian B. Zabel <09zabeli@gmail.com>
2018-12-01 12:26:30 -0800

    Creation of initial Ruby IO test.

---
+++ Keyvalue.rb
@@ -0,0 +1,55 @@
+# key-value
+
+class Keyvalue
+  
+  def start(input=$stdin)
+    
+    @i = 1
+    @dict = {}
+
+    while @i == 1
+
+      print '> '
+      $stdin = @input = input.gets.chomp.split{" "}
+
+      # Determine output based on command (@input[0])
+      if @input[0].downcase == "put"
+
+        @dict.store(@input[1], @input[2])
+        disp = "ok"
+
+      elsif @input[0].downcase == "fetch"
+
+        if @dict.key?(@input[1]) == true
+          
+          disp = @dict[@input[1]].to_s  # Convert hashmap value to string if not already
+        
+        else
+          
+          disp = "value not found"
+        
+        end
+          
+      elsif @input[0].downcase == "exit"
+        
+        disp = "Bye!"
+        @i = 0  # Break the loop
+          
+      else
+
+        # Error message if command (input[0]) not recognized
+        disp = "Unknown command. Known commands are: put, fetch, exit"
+      
+      end
+
+      puts disp
+
+    end
+
+    return disp
+
+  end
+
+end
+
+Keyvalue.new.start
