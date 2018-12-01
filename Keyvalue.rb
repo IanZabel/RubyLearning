0c318f1b75c23f8e77712a6fe5f04e87442268cd
Ian B. Zabel <09zabeli@gmail.com>
2018-12-01 13:15:21 -0800

    Update Keyvalue.rb

--- Keyvalue.rb
+++ Keyvalue.rb
@@ -1,53 +1,55 @@
 # key-value
 
 class Keyvalue
-  
-  def start(input=$stdin)
+
+  def useroutput(inputstring, hashmap)
+    
+    @input = inputstring.chomp.split{" "}
+    @i = 1
+    @dict = hashmap
+
+    if @input[0].downcase == "put"
+      @dict.store(@input[1], @input[2])
+      @output = 'ok'
+
+    elsif @input[0].downcase == 'fetch'
+      if @dict.key?(@input[1]) == true
+        @output = @dict[@input[1]].to_s
+
+      else
+        @output = 'value not found'
+
+      end
+    elsif @input[0].downcase == 'exit'
+      @output = 'Bye!'
+      @i = 0
+
+    else
+      @output = 'Unknown command. Known commands are: put, fetch, exit'
+
+    end
+    return @output, @i, @dict
+
+  end
+
+  def mainloop(input = $stdin, output = $stdin)
     
     @i = 1
     @dict = {}
 
     while @i == 1
-
-      print '> '
-      @input = input.gets.chomp.split{" "}
-
-      # Determine output based on command (@input[0])
-      if @input[0].downcase == "put"
-
-        @dict.store(@input[1], @input[2])
-        disp = "ok"
-
-      elsif @input[0].downcase == "fetch"
-
-        if @dict.key?(@input[1]) == true
-          
-          disp = @dict[@input[1]].to_s  # Convert hashmap value to string if not already
-        
-        else
-          
-          disp = "value not found"
-        
-        end
-          
-      elsif @input[0].downcase == "exit"
-        
-        disp = "Bye!"
-        @i = 0  # Break the loop
-          
-      else
-
-        # Error message if command (input[0]) not recognized
-        disp = "Unknown command. Known commands are: put, fetch, exit"
       
-      end
+      print '> '
+      @userinput = input.gets
+      @useroutput = useroutput(@userinput, @dict)
 
-      puts disp
+      puts @useroutput[0]
+      @i = @useroutput[1]
+      @dict = @useroutput[2]
 
     end
-
-    return disp
-
   end
 
 end
+
+Keyvalue.new.mainloop
